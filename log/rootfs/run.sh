281a128ed0f39233266783b745efcf1b53252eac
Thomas Taube <thomas@greenautarky.com>
2025-11-03 13:08:48 +0100

    Removed glanes, Added fluent-bit, Added telegraf and created Base structure

--- log/rootfs/run.sh
+++ log/rootfs/run.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bashio
 # ==============================================================================
-# Home Assistant Community Add-on: Glances
-# Run the Glances add-on
+# Home Assistant Community Add-on: Log
+# Run the Loggers: FluentBit and Telegraf
 # ==============================================================================
 #
 # WHAT IS THIS FILE?!
 #
-# The Glances add-on runs in the host PID namespace, therefore it cannot
+# The Log add-on runs in the host PID namespace, therefore it cannot
 # use the regular S6-Overlay; hence this add-on uses a "old school" script
 # to run; with a couple of "hacks" to make it work.
 # ==============================================================================
 /etc/cont-init.d/banner.sh
-/etc/cont-init.d/glances.sh
-/etc/cont-init.d/nginx.sh
 
-# Start NGiNX
-/etc/services.d/nginx/run &
+set -e
+echo    "Starting Fluent Bit"
+/usr/bin/fluent-bit -c /etc/fluent-bit/fluent-bit.conf
 
-# InfluxDB export
-/etc/services.d/influxdb/run &
+echo "Starting Telegraf"
+exec telegraf --config /etc/telegraf/telegraf.conf
 
-# Start Glances
-exec /etc/services.d/glances/run
